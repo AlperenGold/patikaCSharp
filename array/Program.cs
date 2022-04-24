98a1bd2e14e4a3405839b60bd7bc450ebf992d54
AlperenGold <alprn.gold@gmail.com>
2022-04-24 17:01:54 +0300

    Added: "array" folder.

---
+++ array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = {23, 12, 4, 86, 72, 3, 11, 17};
+            Console.WriteLine("Sırasız liste: ");
+            
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.WriteLine("Sıralı liste:");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //
+            // Sayı dizisi elemanlarını kullanarak 2. indexten itibaren 2 elemanı sıfırlar.
+            Array.Clear(sayiDizisi, 2, 2);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+            //
+            Array.Resize<int>(ref sayiDizisi, 9);
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
